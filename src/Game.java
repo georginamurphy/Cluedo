86c676e48d34fb82c02ab44548d6f8eb33a80810
Tyler <Tyler@10.140.42.171>
2016-08-05 19:29:45 +1200

    Commenting and cleaning code

--- src/Game.java
+++ src/Game.java
@@ -4,17 +4,26 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
- * 
- * 
- *
+ * The game class is what ties everything together and holds a lot
+ * of game logic, and information about the board.  Player's have
+ * a reference to an instance of this game so that they can access
+ * information about the board.
  */
 public class Game {
+	
+	// The board object for this game
 	private Board board;
+	
+	// The solution to this game
 	private Solution solution;
-	private ArrayList<Player> players; // Consist of every player, both human
-										// and non-human
-	public ArrayList<Player> humanPlayers; // Consists of ONLY the human
-											// players
+	
+	// An ArrayList oh players in the game, including non-humans
+	private ArrayList<Player> players;
+	
+	// An ArrayList of only human players
+	public ArrayList<Player> humanPlayers;
+	
+	// A boolean to catch when the game ends, and the loop in run() should terminate
 	private boolean gameEnd;
 
 	// Array Lists of tiles where players sit for each room
@@ -39,17 +48,49 @@ public class Game {
 	private ArrayList<Location> loungeDoors;
 	private ArrayList<Location> diningRoomDoors;
 
+	// An enum representing a direction on the board
 	public enum Direction {
 		UP, DOWN, LEFT, RIGHT
 	}
 
+	/**
+	 * The constructor for a game, takes a board and an ArrayList of players
+	 * 
+	 * @param board - The board for the game
+	 * @param players - The list of all players
+	 */
 	public Game(Board board, ArrayList<Player> players) {
 		this.board = board;
 		this.players = players;
 		this.gameEnd = false;
 
-		initialiseRoomTileLists();
+		// These two method calls initialize information about the board that never changes between games
+		// The information helps when moving player's into and out of rooms
+		initialiseRoomTileLists(); 
 		initialiseDoorLocations();
+	}
+	
+	/**
+	 * Getter for the board field
+	 * @return
+	 */
+	public Board getBoard() {
+		return this.board;
+	}
+	
+	/**
+	 * Getter for the gameEnd field
+	 * @return
+	 */
+	public boolean getGameEnd(){
+		return this.gameEnd;
+	}
+	
+	/**
+	 * Prints the board
+	 */
+	public void printBoard() {
+		System.out.println(this.board.toString());
 	}
 
 	/**
@@ -67,12 +108,14 @@ public class Game {
 	 */
 	public void createSolution(ArrayList<Character> chars, ArrayList<Weapon> weapons, ArrayList<Room> rooms) {
 
+		// Get random indexes in each ArrayList
 		int charIndex = (int) (Math.random() * chars.size());
 		int weaponIndex = (int) (Math.random() * weapons.size());
 		int roomIndex = (int) (Math.random() * rooms.size());
 
 		this.solution = new Solution(weapons.get(weaponIndex), chars.get(charIndex), rooms.get(roomIndex));
 
+		// Remove the cards from the ArrayLists so they are not dealt to players
 		chars.remove(charIndex);
 		weapons.remove(weaponIndex);
 		rooms.remove(roomIndex);
@@ -89,6 +132,7 @@ public class Game {
 	public void dealCards(ArrayList<Character> chars, ArrayList<Weapon> weapons, ArrayList<Room> rooms) {
 		ArrayList<Card> cards = new ArrayList<Card>();
 		ArrayList<Player> realPlayers = new ArrayList<Player>();
+		
 		// Get the real players, and add them to an ArrayList
 		int numOfPlayers = 0;
 		for (Player p : players) {
@@ -97,25 +141,23 @@ public class Game {
 				numOfPlayers++;
 			}
 		}
-		// Set the field that holds all the human players to be the array we
-		// just calculated
+		
+		// Set the field that holds all the human players to be the array we just calculated
 		this.humanPlayers = realPlayers;
-		// Also, intitalize the game field for each player to be this game
-		// object
+		
+		// Also, initialize the game field for each player to be this game object
 		for (Player p : players) {
 			p.setGame(this);
 		}
 
+		// Place all cards into a single ArrayList and shuffle it
 		cards.addAll(chars);
 		cards.addAll(weapons);
 		cards.addAll(rooms);
-
-		// Shuffle the cards
 		Collections.shuffle(cards);
 
 		// Now deal them out to the players, some players WILL have more cards
-		// than others if
-		// the cards dont split amongst the players evenly.
+		// than others if the cards don't split amongst the players evenly.
 		int playerIndex = 0;
 		for (int i = 0; i < cards.size(); i++) {
 			Card card = cards.get(i);
@@ -125,11 +167,6 @@ public class Game {
 				playerIndex = 0;
 			}
 		}
-
-		// For testing
-		// for(Player p : realPlayers){
-		// p.printCards();
-		// }
 	}
 
 	/**
@@ -137,17 +174,16 @@ public class Game {
 	 * a series of methods that sequence the game.
 	 */
 	public void run() {
+		// This while loop keeps the game running until someone wins
 		while (!gameEnd) {
 			for (Player p : humanPlayers) {
+				// A player is dead if they have made an incorrect accusation
 				if (!p.getDead()) {
 					p.startTurn();
-					if (gameEnd) {
-						break;
-					}
+					if (gameEnd) {break;} // Break the for each loop if a player won the game
 				}
 			}
 		}
-
 		System.out.println("The game has been won. The solution was:");
 		System.out.println(solution.toString());
 	}
@@ -168,93 +204,82 @@ public class Game {
 		int playerX = player.getLocation().x;
 		int playerY = player.getLocation().y;
 
-		// Depending on the direction the player wants to move, check if it is
-		// valid
+		// A series of 4 if statements will catch what direction we are needing to check
+		// Appropriate checks will then be made that apply to the piece in that direction from the player
+		
 		if (direction == Direction.UP) {
-			if (playerY == 0) {
-				return false;
-			} // Player can't move off the board
+			if (playerY == 0) {return false;} // Player can't move off the board
 			BoardPiece piece = gameBoard[playerY - 1][playerX];
 
-			if (piece instanceof Hallway) {
-				return true;
-			}
+			if (piece instanceof Hallway) {return true;} // The tile in the given direction is available to move into
+			
+			// If the piece is a RoomTile, they may only move onto it if it is a door that does not belong to the room they were in last turn
 			if (piece instanceof RoomTile) {
 				if (piece != null) {
 					if (((RoomTile) piece).getName().equals(player.getRoomLastTurn())) {
 						System.out.println("You can't enter that room! You were there during your last turn");
 						return false;
-					} else {
-						return roomTileCheck(player, direction);
-					}
+					} 
+					else {return isRoomDoor(player, direction);}
 				}
 			}
-		} else if (direction == Direction.DOWN) {
-			if (playerY == 24) {
-				return false;
-			} // Player can't move off the board
+		} 
+		
+		
+		else if (direction == Direction.DOWN) {
+			if (playerY == 24) {return false;} // Player can't move off the board
 			BoardPiece piece = gameBoard[playerY + 1][playerX];
 
-			if (piece instanceof Hallway) {
-				return true;
-			}
+			if (piece instanceof Hallway) {return true;} // The tile in the given direction is available to move into
+			
+			// If the piece is a RoomTile, they may only move onto it if it is a door that does not belong to the room they were in last turn
 			if (piece instanceof RoomTile) {
 				if (piece != null) {
 					if (((RoomTile) piece).getName().equals(player.getRoomLastTurn())) {
 						System.out.println("You can't enter that room! You were there during your last turn");
 						return false;
-					} else {
-						return roomTileCheck(player, direction);
-					}
+					} 
+					else {return isRoomDoor(player, direction);}
 				}
-			} else {
-				return roomTileCheck(player, direction);
-			}
-
-		} else if (direction == Direction.LEFT) {
-			if (playerX == 0) {
-				return false;
-			} // Player can't move off the board
+			} 
+		} 
+		
+		
+		else if (direction == Direction.LEFT) {
+			if (playerX == 0) {return false;} // Player can't move off the board
 			BoardPiece piece = gameBoard[playerY][playerX - 1];
 
-			if (piece instanceof Hallway) {
-				return true;
-			}
+			if (piece instanceof Hallway) {return true;}
+			
+			// If the piece is a RoomTile, they may only move onto it if it is a door that does not belong to the room they were in last turn
 			if (piece instanceof RoomTile) {
 				if (piece != null) {
 					if (((RoomTile) piece).getName().equals(player.getRoomLastTurn())) {
 						System.out.println("You can't enter that room! You were there during your last turn");
 						return false;
-					} else {
-						return roomTileCheck(player, direction);
-					}
+					} 
+					else {return isRoomDoor(player, direction);}
 				}
-			} else {
-				return roomTileCheck(player, direction);
-			}
-
-		} else if (direction == Direction.RIGHT) {
-			if (playerX == 24) {
-				return false;
-			} // Player can't move off the board
+			} 
+		} 
+		
+		
+		else if (direction == Direction.RIGHT) {
+			if (playerX == 24) {return false;} // Player can't move off the board
 			BoardPiece piece = gameBoard[playerY][playerX + 1];
 
-			if (piece instanceof Hallway) {
-				return true;
-			}
+			if (piece instanceof Hallway) {return true;}
+			
+			// If the piece is a RoomTile, they may only move onto it if it is a door that does not belong to the room they were in last turn
 			if (piece instanceof RoomTile) {
 				if (piece != null) {
 					if (((RoomTile) piece).getName().equals(player.getRoomLastTurn())) {
 						System.out.println("You can't enter that room! You were there during your last turn");
 						return false;
-					} else {
-						return roomTileCheck(player, direction);
-					}
+					} 
+					else {return isRoomDoor(player, direction);}
 				}
-			} else {
-				return roomTileCheck(player, direction);
-			}
-
+			} 
 		}
 		return false;
 	}
@@ -262,18 +287,28 @@ public class Game {
 	/**
 	 * Applies a given move to the board
 	 * 
-	 * @param player
-	 * @param direction
+	 * @param player - The player to apply the move to
+	 * @param direction - The direction a player must move
 	 */
 	public void applyMove(Player player, Direction direction) {
 		int playerX = player.getLocation().x;
 		int playerY = player.getLocation().y;
 		BoardPiece piece;
 
+		// Given a Direction the player is moving in, it will shift the player on the board
+		// It detects if the player has just moved onto a door, meaning they now need to be
+		// moved to one of the designated locations within the room for players to sit
+		// The 2D Array that represents the board is only updated at the end of this method
+		
+		// The first case's comment apply to the remaining cases as well
+		
 		switch (direction) {
 		case UP:
+			// Shift the player's location in the appropriate direction, note the board is not physically updated yet
 			player.getLocation().moveUp();
-			piece = this.board.getBoard()[playerY - 1][playerX];
+			
+			// If the player moved onto a RoomTile that is a door, update their location to a designated spot in the room
+			piece = this.board.getBoard()[playerY - 1][playerX]; System.out.println(piece);
 			if (piece instanceof RoomTile) {
 				Room.Name name = ((RoomTile) piece).getName();
 				player.updateLocation(getRoomTile(name));
@@ -281,7 +316,7 @@ public class Game {
 			break;
 		case DOWN:
 			player.getLocation().moveDown();
-			piece = this.board.getBoard()[playerY + 1][playerX];
+			piece = this.board.getBoard()[playerY + 1][playerX]; System.out.println(piece);
 			if (piece instanceof RoomTile) {
 				Room.Name name = ((RoomTile) piece).getName();
 				player.updateLocation(getRoomTile(name));
@@ -289,7 +324,7 @@ public class Game {
 			break;
 		case RIGHT:
 			player.getLocation().moveRight();
-			piece = this.board.getBoard()[playerY][playerX + 1];
+			piece = this.board.getBoard()[playerY][playerX + 1]; System.out.println(piece);
 			if (piece instanceof RoomTile) {
 				Room.Name name = ((RoomTile) piece).getName();
 				player.updateLocation(getRoomTile(name));
@@ -297,7 +332,7 @@ public class Game {
 			break;
 		case LEFT:
 			player.getLocation().moveLeft();
-			piece = this.board.getBoard()[playerY][playerX - 1];
+			piece = this.board.getBoard()[playerY][playerX - 1]; System.out.println(piece);
 			if (piece instanceof RoomTile) {
 				Room.Name name = ((RoomTile) piece).getName();
 				player.updateLocation(getRoomTile(name));
@@ -305,18 +340,18 @@ public class Game {
 			break;
 		}
 
-		board.updateBoard(players);
+		board.updateBoard(players); // Physically updates the board
 	}
 
 	/**
 	 * Checks if a given tile in the direction FROM the player is a room tile
 	 * that represents a door
 	 * 
-	 * @param player
-	 * @param direction
+	 * @param player - The player we are checking for a door from
+	 * @param direction - The direction from the player where we want to check
 	 * @return
 	 */
-	public boolean roomTileCheck(Player player, Direction direction) {
+	public boolean isRoomDoor(Player player, Direction direction) {
 		int playerX = player.getLocation().getX();
 		int playerY = player.getLocation().getY();
 
@@ -435,12 +470,11 @@ public class Game {
 			}
 			break;
 		}
-		System.out.println("SOMETHING WENT VERY BAD");
-		return null; // SHOULD NEVER HAPPEN
+		return null; // This should never happen
 	}
 
 	/**
-	 * returns true if the player p is in a room
+	 * Returns true if the player p is in a room
 	 * 
 	 * @param player
 	 * @return boolean
@@ -457,6 +491,8 @@ public class Game {
 		roomTileLists.add(loungeTiles);
 		roomTileLists.add(diningRoomTiles);
 
+		// Will return true if a player's location is equal to one of 
+		// the 6 designated spots in each room for players to sit in.
 		for (ArrayList<Location> list : roomTileLists) {
 			if (list.contains(p.getLocation())) {
 				return true;
@@ -465,6 +501,12 @@ public class Game {
 		return false;
 	}
 
+	/**
+	 * Return's the name of a room that a player sit's in
+	 * 
+	 * @param p
+	 * @return
+	 */
 	public Room.Name inRoom(Player p) {
 		if (kitchenTiles.contains(p.getLocation()))
 			return Room.Name.KITCHEN;
@@ -485,80 +527,72 @@ public class Game {
 		if (diningRoomTiles.contains(p.getLocation()))
 			return Room.Name.DININGROOM;
 
-		System.out.println("SOMETHING WENT HORRIBLY WRONG");
-		return null;
+		return null; // Should never do this
 	}
 
+	/**
+	 * Checks whether a room has a free door to exit from
+	 * 
+	 * @param name - The name of the room we wish to inspect
+	 * @return
+	 */
 	public boolean hasFreeDoor(Room.Name name) {
 		switch (name) {
 		case KITCHEN:
 			for (Location l : kitchenDoors) {
-				if (isFreeDoor(l) != null) {
-					return true;
-				}
+				if (isFreeDoor(l) != null) {return true;}
 			}
 			return false;
 		case BALLROOM:
 			for (Location l : ballRoomDoors) {
-				if (isFreeDoor(l) != null) {
-					return true;
-				}
+				if (isFreeDoor(l) != null) {return true;}
 			}
 			return false;
 		case CONSERVATORY:
 			for (Location l : conservatoryDoors) {
-				if (isFreeDoor(l) != null) {
-					return true;
-				}
+				if (isFreeDoor(l) != null) {return true;}
 			}
 			return false;
 		case BILLIARD:
 			for (Location l : billiardDoors) {
-				if (isFreeDoor(l) != null) {
-					return true;
-				}
+				if (isFreeDoor(l) != null) {return true;}
 			}
 			return false;
 		case LIBRARY:
 			for (Location l : libraryDoors) {
-				if (isFreeDoor(l) != null) {
-					return true;
-				}
+				if (isFreeDoor(l) != null) {return true;}
 			}
 			return false;
 		case STUDY:
 			for (Location l : studyDoors) {
-				if (isFreeDoor(l) != null) {
-					return true;
-				}
+				if (isFreeDoor(l) != null) {return true;}
 			}
 			return false;
 		case HALL:
 			for (Location l : hallDoors) {
-				if (isFreeDoor(l) != null) {
-					return true;
-				}
+				if (isFreeDoor(l) != null) {return true;}
 			}
 			return false;
 		case LOUNGE:
 			for (Location l : loungeDoors) {
-				if (isFreeDoor(l) != null) {
-					return true;
-				}
+				if (isFreeDoor(l) != null) {return true;}
 			}
 			return false;
 		case DININGROOM:
 			for (Location l : diningRoomDoors) {
-				if (isFreeDoor(l) != null) {
-					return true;
-				}
+				if (isFreeDoor(l) != null) {return true;}
 			}
 			return false;
 		}
-		System.out.println("SHOULDNT HAVE GOT HERE LOL");
-		return false;
+		return false; // Should not get here
 	}
 
+	/**
+	 * Will check if the given door is not blocked by a player and can be moved through
+	 * 
+	 * @param doorLocation
+	 * @return
+	 */
 	public Location isFreeDoor(Location doorLocation) {
 		Location left = new Location(doorLocation.x - 1, doorLocation.y);
 		Location right = new Location(doorLocation.x + 1, doorLocation.y);
@@ -584,6 +618,12 @@ public class Game {
 		return null;
 	}
 
+	/**
+	 * Returns the ArrayList of doors for a given room
+	 * 
+	 * @param name
+	 * @return
+	 */
 	public ArrayList<Location> getDoorLocations(Room.Name name) {
 		switch (name) {
 		case KITCHEN:
@@ -605,10 +645,15 @@ public class Game {
 		case DININGROOM:
 			return this.diningRoomDoors;
 		}
-		System.out.println("HOLY SOMEWTHING WENT REALLY WRONG");
-		return null;
+		return null; // Should never return null
 	}
 
+	/**
+	 * Returns a character color given an integer
+	 * 
+	 * @param num
+	 * @return
+	 */
 	public Character.Colour getCharacterColour(int num) {
 		switch (num) {
 		case 1:
@@ -627,6 +672,12 @@ public class Game {
 		return null;
 	}
 	
+	/**
+	 * Returns a room name given an integer
+	 * 
+	 * @param num
+	 * @return
+	 */
 	public Room.Name getRoomName(int num) {
 		switch (num) {
 		case 1:
@@ -651,6 +702,12 @@ public class Game {
 		return null;
 	}
 
+	/**
+	 * Returns a weapon name given an integer
+	 * 
+	 * @param num
+	 * @return
+	 */
 	public Weapon.Type getWeaponName(int num) {
 		switch (num) {
 		case 1:
@@ -669,10 +726,15 @@ public class Game {
 		return null;
 	}
 
+	/**
+	 * The initial method that starts the suggestion / accusation phase of a player's turn.
+	 * 
+	 * @param p - The player who's turn it is currently
+	 */
 	public void makeSuggestionDecisions(Player p) {
 		Scanner input = new Scanner(System.in);
 		printBoard();
-		System.out.println("You have entered a room you can make eiter a suggestion or an accusation");
+		System.out.println("You have entered a room you can make either a suggestion or an accusation");
 		System.out.println("Press 1 for a suggestion or 2 for an accusation");
 
 		int userInput = getUserInput(input, 1, 2);
@@ -685,7 +747,6 @@ public class Game {
 		case 2:
 			makeAccusation(p, input);
 			break;
-
 		}
 
 	}
@@ -831,6 +892,14 @@ public class Game {
 		}
 		return userInput;
 	}
+	
+	
+	// ===================================================================================================================================================================
+	//
+	//                  The methods beyond this point are to initialize specific information about the board that does not change between games.
+	//                      They provide information that is mainly used when moving players to and from the different rooms in the game.
+	//
+	// ===================================================================================================================================================================
 
 	/**
 	 * Initializes the ArrayLists for the room tiles that players will sit in if
@@ -972,37 +1041,6 @@ public class Game {
 					}
 				}
 			}
-		}
-	}
-
-	public Board getBoard() {
-		return this.board;
-	}
-	
-	public boolean getGameEnd(){
-		return this.gameEnd;
-	}
-
-	/**
-	 * Prints the board
-	 */
-	public void printBoard() {
-		System.out.println(this.board.toString());
-	}
-
-	public void mockGame(ArrayList<Character> characters, ArrayList<Weapon> weapons, ArrayList<Room> rooms) {
-		solution = new Solution(new Weapon(Weapon.Type.ROPE), new Character(Character.Colour.BLUE),
-				new Room(Room.Name.BALLROOM));
-
-	}
-
-	public void printDoors(Room.Name name) {
-		switch (name) {
-		case BALLROOM:
-			for (Location l : ballRoomDoors) {
-				System.out.println(l.x + " " + l.y);
-			}
-			break;
 		}
 	}
 }
